0f5f49354ba7f291a07a87a46e9feae51685fdc7
Torsten Compart <torsten.compart@gmail.com>
2013-11-28 00:40:58 +0100

    Adding functionality to webapp.js using some meteor features like templates, listeners, sessions
    Html structure changed therefore a little with a button navigation bar.
    The categories can be added using the plus sign.

--- webapp.js
+++ webapp.js
@@ -1,15 +1,43 @@
+/*global Meteor, Session, Template */
+var lists = new Meteor.Collection("Lists");
+
+/////Generic Helper Functions/////
+/////Generic Helper Functions/////
+//this function puts our cursor where it needs to be.
+function focusText(input) {
+  input.focus();
+  input.select();
+}
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to webapp.";
+  Template.categories.lists = function () {
+    return lists.find({}, {sort: {Category: 1}});
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.categories.new_category = function () {
+    return Session.equals('adding_category', true);
+  };
+
+  Template.categories.events({
+    'click #btnNewCategory': function (event, template) {
+      Session.set('adding_category', true);
+      Meteor.flush();
+      focusText(template.find("#add-category"));
+    },
+    'keyup #add-category': function (event, template) {
+      if (event.which === 13) {
+        var categoryValue = String(event.target.value || "");
+        if (categoryValue) {
+          lists.insert({Category: categoryValue});
+          Session.set('adding_category', false);
+        }
+      }
+    },
+    'focusout #add-category': function (event, template) {
+      Session.set('adding_category', false);
     }
   });
+  Session.set('adding_category', false);
 }
 
 if (Meteor.isServer) {
