0728ef5efdde17f0d08127ece915bc951318bcf3
Torsten Compart <torsten.compart@gmail.com>
2014-02-03 00:22:44 +0100

    grunt support for webapp project
    Adding grunt-express-server and environment depending configurations.
    This way debugging and live reloading while developing is possible.

--- gruntfile.js
+++ gruntfile.js
@@ -1,19 +1,15 @@
 /*global module:false*/
 'use strict';
 
-var request = require('request');
-
 module.exports = function (grunt) {
   // show elapsed time at the end
   require('time-grunt')(grunt);
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
 
   var reloadPort = 35729, files;
 
   grunt.initConfig({
     jshint: {
-      src: ['gruntfile.js', 'app/**/*.js', 'config/**/*.js', 'tests/app/**/*.js'],
+      src: ['gruntfile.js', 'app/**/*.js', 'routes/**/*.js', 'server/**/*.js', 'tests/app/**/*.js'],
       options: {
         curly: true,
         eqeqeq: true,
@@ -41,53 +37,42 @@ module.exports = function (grunt) {
       }
     },
     pkg: grunt.file.readJSON('package.json'),
-    develop: {
-      server: {
-        file: 'app.js',
-        env: { NODE_ENV: 'development'}
+    express: {
+      options: {
+        script: 'server/server.js',
+        background: false,
+        debug: true,
+        output: ".+"
+      },
+      dev: {
+        options: {
+          node_env: 'development'
+        }
+      },
+      prod: {
+        options: {
+          node_env: 'production'
+        }
+      },
+      test: {
+        options: {
+          node_env: 'test'
+        }
       }
     },
     watch: {
-      options: {
-        nospawn: true,
-        livereload: reloadPort
-      },
-      js: {
-        files: [
-          'app.js',
-          'app/**/*.js',
-          'config/*.js'
-        ],
-        tasks: ['develop', 'delayed-livereload']
-      },
-      jade: {
-        files: ['app/views/**/*.jade'],
-        options: { livereload: reloadPort }
+      express: {
+        files:  [ 'public/**/*.js', 'server/**', 'routes/**' ],
+        tasks:  [ 'express:dev' ],
+        options: {
+          spawn: false
+        }
       }
     }
   });
 
-  grunt.config.requires('watch.js.files');
-  files = grunt.config('watch.js.files');
-  files = grunt.file.expand(files);
-
-  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
-    var done = this.async();
-    setTimeout(function () {
-      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function (err, res) {
-        var reloaded = !err && res.statusCode === 200;
-        if (reloaded) {
-          grunt.log.ok('Delayed live reload successful.');
-        } else {
-          grunt.log.error('Unable to make a delayed live reload.');
-        }
-        done(reloaded);
-      });
-    }, 500);
-  });
-
+  grunt.loadNpmTasks('grunt-express-server');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-develop');
 
-  grunt.registerTask('default', ['develop', 'watch']);
+  grunt.registerTask('server', ['express:dev', 'watch']);
 };
